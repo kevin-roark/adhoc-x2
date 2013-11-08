e1ea542d22bcfeb0481a687aa67bf54e23f9ad37
Kevin Roark <ker2143@columbia.edu>
2013-11-08 15:30:19 -0500

    fixin' serch stuff?

--- blog/search_indexes.py
+++ blog/search_indexes.py
@@ -5,5 +5,14 @@ from blog.models import Post
 
 class PostIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
+    published = BooleanField(model_attr='published')
+    timestamp = DateTimeField(model_attr='timestamp')
+
+    def index_queryset(self):
+        """
+        Called when the entire search index is updated.
+        aka objects returned can be searched
+        """
+        return Post.objects.filter(published=True)
 
 site.register(Post, PostIndex)
