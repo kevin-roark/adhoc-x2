77440c4134daed30e9d5556115eb425a48bc8c61
Kevin Roark <ker2143@columbia.edu>
2014-01-24 13:01:15 -0500

    debugging ads

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   // serve ads
   $.post("/ads/serve/", {slots: "[1, 2, 3]"}, function(data) {
+    console.log('got response from ads request');
     for (i=0;i<data.length;i++) {
-      
       $("#ad_slot_" + data[i]['slot']).html(data[i]['code']).addClass("inserted");
+      console.log('added ad ' + i);
     }
   });
 
