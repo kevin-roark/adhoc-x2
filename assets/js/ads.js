5e9bd0d383c7ac7d648815bad2edfc93114016e2
Kevin Roark <ker2143@columbia.edu>
2014-02-14 12:44:44 -0500

    removed console log from ads

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   // serve ads
   $.post("/ads/serve/", {slots: "[1, 2, 3, 4, 5]"}, function(data) {
-    console.log('got response from ads request');
     for (i=0;i<data.length;i++) {
       $("#ad_slot_" + data[i]['slot']).html(data[i]['code']).addClass("inserted");
     }
