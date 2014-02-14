6f3e9dd2724e716128de9a5faea7ba8a5ef762c0
Kevin Roark <ker2143@columbia.edu>
2014-02-14 16:14:57 -0500

    Calendar redesign, mainly

--- adhoc_calendar/templatetags/events.py
+++ adhoc_calendar/templatetags/events.py
@@ -8,37 +8,49 @@ register = template.Library()
 
 @register.simple_tag
 def event_box(event):
+    all_html = []
+
     open_html = '<div class="event-box'
     if (event.url):
         open_html += ' event-box-link"><a href="' + event.url + '" target="_blank"</a>'
     else:
         open_html += '">'
-    all_html = [open_html]
+    all_html.append(open_html)
 
-    if False:#(event.url):
-        title = '<div class="event-title"><a target="_blank" href="' + event.url + '">' + event.title + '</a></div>'
-    else:
-        title = '<div class="event-title">' + event.title + '</div>'
+    month = event.start_time.strftime('%b');
+    dow = event.start_time.strftime('%a');
+    day = event.start_time.strftime('%d');
+    badge = '<div class="event-date-badge">' + dow + '<hr>' + month + ' ' + day + '</div>'
+    all_html.append(badge);
+
+    title = '<div class="event-title">' + event.title + '</div>'
     all_html.append(title)
 
     when = '<div class="event-time">' + event.start_time.strftime('%B %d. %I:%M %p')
     if (event.end_time):
-        when += event.end_time.strftime('- %I:%M %p')
+        when += event.end_time.strftime(' - %I:%M %p')
     when += '</div>'
     all_html.append(when)
 
     where = '<div class="event-venue">@ ' + event.venue + '</div>'
     all_html.append(where)
 
+    if (event.url):
+        link = '<div class="event-url">' + event.url + '</div>'
+        all_html.append(link);
+
+    if (event.details or event.image):
+        all_html.append('<hr>')
+
     if (event.image):
-        image = '<div class="event-image"><img src="' + event.image.default_image.url + '" alt="' + event.image.title + ">"
+        image = '<div class="event-image"><img src="' + event.image.default_image.url + '" alt="' + event.image.title + '">'
         if (event.image.caption):
-            image += '<p>' + event.image.caption + '</p>'
+          image += '<div class="event-image-caption">' + event.image.caption + '</div>'
         image += '</div>'
         all_html.append(image)
 
     if (event.details):
-        details = '<hr><div class="event-description">' + event.details + '</div>'
+        details = '<div class="event-description">' + event.details + '</div>'
         all_html.append(details)
 
     if (event.url):
