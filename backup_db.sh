d2c6ce80c39a2e114465bc599f35c4ed3efce58a
Kevin Roark <ker2143@columbia.edu>
2014-03-07 14:53:20 -0500

    nice script updates

--- backup_db.sh
+++ backup_db.sh
@@ -1,1 +1,16 @@
-sudo -u postgres pg_dump 
+#!/bin/bash
+now=$(date +"%m_%d_%Y")
+fname="adhoc-data-backup-$now.sql"
+
+echo "doing backup to $fname"
+sudo -u postgres pg_dump adhocdb > $fname
+echo "backup complete"
+
+echo "uploading backup to cloudup"
+up $fname
+wait
+echo "upload complete"
+
+echo "deleting local file"
+rm $fname
+echo "file deleted, bye bye"
